a92a055314c55f0097c8fbd0f6dd44acf6c46cbd
Vikyath  Naradasi <vikyath@Vikyaths-MacBook-Pro.local>
2024-01-01 22:56:24 -0800

    cahnged IDs

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask, request, jsonify
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
 import requests
+import config
 
 app = Flask(__name__)
 
-# Spotify API credentials (Client ID and Client Secret)
-SPOTIPY_CLIENT_ID = '32fa381477cc42a98058658debebe637'
-SPOTIPY_CLIENT_SECRET = '743df42b17c34f7887d94349c147ca60'
-
 # Initialize the Spotify client
-client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=config.SPOTIPY_CLIENT_ID, 
+    client_secret=config.SPOTIPY_CLIENT_SECRET
+)
 spotify = Spotify(client_credentials_manager=client_credentials_manager)
 
-# Weather API key
-WEATHER_API_KEY = 'a19185f10075ce7c80e43d04557895e8'
 
 @app.route('/get_weather', methods=['POST'])
 def get_weather():
@@ -24,7 +22,7 @@ def get_weather():
         zip_code = data.get('zip_code')
 
         # Fetch weather data from the weather API
-        weather_url = f'https://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={WEATHER_API_KEY}'
+        weather_url = f'https://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={config.WEATHER_API_KEY}'
         response = requests.get(weather_url)
         weather_data = response.json()
 
